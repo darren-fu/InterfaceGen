eb64ee423a39cd37e7af8e2d8b97ce9b68a93da7
darren-fu <tiger_kin@163.com>
2017-01-12 17:33:18 +0800

    aa

---
+++ src/main/java/df/open/annotation/GenerateInterface.java
@@ -0,0 +1,25 @@
+package df.open.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 说明:
+ * <p/>
+ * Copyright: Copyright (c)
+ * <p/>
+ * Company: 江苏千米网络科技有限公司
+ * <p/>
+ *
+ * @author 付亮(OF2101)
+ * @version 1.0.0
+ * @date 2016/10/11
+ */
+@Target(ElementType.TYPE)//注解使用目标为类
+@Retention(RetentionPolicy.SOURCE)//注解保留范围为源代码
+public @interface GenerateInterface {
+    String suffix() default "Interface";//生成对应接口的后缀名
+
+}
---
+++ src/main/java/df/open/annotation/TestService.java
@@ -0,0 +1,27 @@
+package df.open.annotation;
+
+/**
+ * 说明:
+ * <p/>
+ * Copyright: Copyright (c)
+ * <p/>
+ * Company: 江苏千米网络科技有限公司
+ * <p/>
+ *
+ * @author 付亮(OF2101)
+ * @version 1.0.0
+ * @date 2016/10/11
+ */
+//@GenerateInterface
+public class TestService {
+    //教书
+    private void teach(){
+        System.out.println("teach...");
+    }
+
+    //行走
+    public void walk(){
+        System.out.println("walking");
+    }
+
+}
